d0026938a3b77c46d5fc3fd1e3786ded33161f34
rakshithahs03 <itsrakshithahs@gmail.com>
2025-10-27 23:59:56 +0530

    First Commit

---
+++ budgetwise-backend/src/main/java/com/budgetwise/budgetwise_backend/BudgetwiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.budgetwise.budgetwise_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetwiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetwiseBackendApplication.class, args);
+	}
+
+}
---
+++ budgetwise-backend/src/test/java/com/budgetwise/budgetwise_backend/BudgetwiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetwise.budgetwise_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetwiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
